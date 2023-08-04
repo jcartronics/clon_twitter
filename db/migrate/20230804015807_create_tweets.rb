18bda690b00bdde087a55f6109bba664c115f45b
jcartronics <jcar-95@hotmail.com>
2023-08-03 23:21:51 -0400

    Agrega validaciones en base de datos

--- db/migrate/20230804015807_create_tweets.rb
+++ db/migrate/20230804015807_create_tweets.rb
@@ -1,8 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
-      t.text :description
-      t.string :userName
+      t.text :description, null: false
+      t.string :userName, null: false
 
       t.timestamps
     end
